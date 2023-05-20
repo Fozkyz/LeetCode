842e27fa57e1692e819e0b79c01f3efe3d9e4466
Paul Nivoix <fozkyz@gmail.com>
2023-05-20 13:03:50 +0200

    Added palindrome linked list problem solver

--- LeetCode/LeetCode/LeetCode.cpp
+++ LeetCode/LeetCode/LeetCode.cpp
@@ -1,10 +1,12 @@
 #include <iostream>
 #include "13.Roman_To_Integer.h"
+#include "234.Palindrome_Linked_List.h"
 
 int main()
 {
-	RomanToInteger::RomanToIntegerProblem* problem = new RomanToInteger::RomanToIntegerProblem("MMCMXIV");
-	
+	//RomanToInteger::RomanToIntegerProblem* problem = new RomanToInteger::RomanToIntegerProblem("MMCMXIV");
+	PalindromeLinkedList::PalindromeLinkedListProblem* problem = new PalindromeLinkedList::PalindromeLinkedListProblem();
+
 	problem->PrintSolution();
 
 	delete problem;
